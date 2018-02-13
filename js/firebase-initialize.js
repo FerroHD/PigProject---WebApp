bdc0ebeb9ede8fd148d80f820e6531eb95659bb7
Alessandro <aleferrara1998@gmail.com>
2018-02-13 21:05:06 +0100

    remove firebase

--- js/firebase-initialize.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    apiKey: "AIzaSyBx7NYduT8eEwPoY8M5RPWUXpJvyhZqcyQ",
-    authDomain: "pigproject-control-panel.firebaseapp.com",
-    databaseURL: "https://pigproject-control-panel.firebaseio.com",
-    projectId: "pigproject-control-panel",
-    storageBucket: "pigproject-control-panel.appspot.com",
-    messagingSenderId: "1094428115590"
-  };
-  firebase.initializeApp(config);
